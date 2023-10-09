e1ebdcb8d7d5e06688f78b698921d297659755e0
Alan Sandoval <alan.sandoval@liberas.com>
2023-10-09 23:00:05 +0000

    add filtering by parent process

--- main.go
+++ main.go
@@ -29,6 +29,12 @@ func main() {
 		verbose = true
 	}
 
+	parentRoot := false
+	parentUid := os.Getenv("ROOTPROCESSPARENT")
+	if parentUid == "true" {
+		parentRoot = true
+	}
+
 	var reloadSignal syscall.Signal
 	reloadSignalStr := os.Getenv("RELOAD_SIGNAL")
 	if reloadSignalStr == "" {
@@ -41,7 +47,7 @@ func main() {
 		}
 	}
 
-	log.Printf("starting reloader with CONFIG_DIR=%s, PROCESS_NAME=%s, RELOAD_SIGNAL=%s\n", configDir, processName, reloadSignal)
+	log.Printf("starting reloader with CONFIG_DIR=%s, PROCESS_NAME=%s, ROOTPROCESSPARENT=%t, RELOAD_SIGNAL=%s\n", configDir, processName, parentRoot, reloadSignal)
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
@@ -61,7 +67,7 @@ func main() {
 				}
 				if event.Op&fsnotify.Chmod != fsnotify.Chmod {
 					log.Println("modified file:", event.Name)
-					err := reloadProcesses(processName, reloadSignal)
+					err := reloadProcesses(processName, parentRoot, reloadSignal)
 					if err != nil {
 						log.Println("error:", err)
 					}
@@ -86,7 +92,7 @@ func main() {
 	<-done
 }
 
-func findPIDs(process string) ([]int, error) {
+func findPIDs(process string, parent bool) ([]int, error) {
 	processes, err := ps.Processes()
 	if err != nil {
 		return nil, fmt.Errorf("failed to list processes: %v\n", err)
@@ -95,9 +101,18 @@ func findPIDs(process string) ([]int, error) {
 	var similar_processes []int
 	for _, p := range processes {
 		// log.Printf("found processes %v", p.Executable())
+		// log.Printf("with parent process: %v", p.PPid())
 		if p.Executable() == process {
 			log.Printf("found executable %s (pid: %d)\n", p.Executable(), p.Pid())
-			similar_processes = append(similar_processes, p.Pid())
+			if parent {
+				if p.PPid() == 0 { // user root is UID 0
+					log.Printf("found command controlled by root!")
+					similar_processes = append(similar_processes, p.Pid())
+				}
+			} else {
+				log.Printf("Parent process not taken into account!")
+				similar_processes = append(similar_processes, p.Pid())
+			}
 		}
 	}
 	if len(similar_processes) > 0 {
@@ -108,8 +123,8 @@ func findPIDs(process string) ([]int, error) {
 	return nil, fmt.Errorf("no process matching %s found\n it may still be starting...", process) // TODO: this should be a WARNING not an ERROR
 }
 
-func reloadProcesses(process string, signal syscall.Signal) error {
-	pids, err := findPIDs(process)
+func reloadProcesses(process string, parent bool, signal syscall.Signal) error {
+	pids, err := findPIDs(process, parent)
 	if err != nil {
 		return err
 	}
